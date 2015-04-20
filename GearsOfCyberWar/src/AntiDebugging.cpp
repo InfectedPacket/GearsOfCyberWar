9b060b828d463292d618cc255699533372d1fa93
Infected Packet <infectedpacket@gmail.com>
2015-04-20 11:49:34 -0700

    Added PEB Checkup for AD
    Checks for the PEB byte to detect attached debugger.

--- GearsOfCyberWar/src/AntiDebugging.cpp
+++ GearsOfCyberWar/src/AntiDebugging.cpp
@@ -1,8 +1,19 @@
 
 #include <stdio.h>
 
-/**
-
-int3 tactic
-
-*/
+bool IsBeingDebugByteSetInPEB() {
+	unsigned char IsDebuggerPresent = 0;
+    __asm
+    {
+            mov eax,dword ptr fs:[0x18]
+            mov eax,dword ptr [eax+0x30]
+            cmp byte ptr [eax+2],0
+            je NoDebugger
+            mov IsDebuggerPresent, 1
+			jmp ReturnToMain
+    NoDebugger:
+            mov IsDebuggerPresent, 0
+	ReturnToMain:
+    }
+	return IsDebuggerPresent;
+}
